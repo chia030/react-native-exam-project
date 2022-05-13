b8c25bb08560b4b73361170c06e2f608bda24d17
chia <chiaravisca@outlook.com>
2022-05-13 20:02:37 +0200

    Added dependencies and react query.

--- App.tsx
+++ App.tsx
@@ -5,23 +5,29 @@ import ReduxThunk from 'redux-thunk';
 import Navigation from './components/Navigation';
 import chatReducer from './store/reducers/chat.reducer';
 import userReducer from './store/reducers/user.reducer';
+import postReducer from './store/reducers/post.reducer';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 
 
 const rootReducer = combineReducers({
   chat: chatReducer,
   user: userReducer,
-  // posts: PostReducer
+  posts: postReducer
 });
 export type RootState = ReturnType<typeof rootReducer>
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Navigation />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
+    </QueryClientProvider>
   )
 }
 
